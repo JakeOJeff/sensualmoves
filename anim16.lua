d63ffcd47a88dc20e158921a559db471ab77d6bf
JakeOJeff <87922888+JakeOJeff@users.noreply.github.com>
2023-03-27 00:07:00 +0530

    Update anim16.lua

--- anim16.lua
+++ anim16.lua
@@ -3,30 +3,39 @@ anim16 = class("anim")
 local anim16s = {}
 local lg = love.graphics
 
+-- ==================================================================
+-- Animation Initialization
 function anim16:initialize(position,frames,speed,x,y,rotation, sx, sy)
-    self.position = position
-    self.frames = frames
-    self.speed = speed or 8
 
-    self.x = x
+    -- Declaring Position, Frames, Speed
+    self.position = position -- Location of the image
+    self.frames = frames -- Max frames
+    self.speed = speed or 8 -- Speed of the Animation / cFrame switching speed
+
+    -- Image arguments
+    self.x = x 
     self.y = y
     self.r = rotation or 0
     self.sx = sx or 1
     self.sy = sy or 1
 
+    -- Loading the Image
     self.table = {}
     for i = 1,self.frames,1 do
-        self.table[i] = love.graphics.newImage(self.position..i..".png")
+        self.table[i] = love.graphics.newImage(self.position..i..".png") -- Loading based on the number of each frame
     end
 
-    self.n = 1
-    self.c = self.table[self.n]
+    self.n = 1 -- Frame ID
+    self.c = self.table[self.n] -- Frame Image
 
     table.insert(anim16s, self)
 	return self
 end
+
+-- ==================================================================
+-- Switching cFrames
 function anim16:play(p, dt)
-    local p = p or true
+    local p = p or true -- Enabling
     if p then 
         if self.n < #self.table then
             self.n = self.n + self.speed * dt
@@ -36,10 +45,14 @@ function anim16:play(p, dt)
             self.c = self.table[self.n]
         end
     end
-    return math.floor(self.n),self.c
+    return math.floor(self.n),self.c -- Returning ID & Image
     
 end
+
+-- ==================================================================
+-- Updating animations on Image types
 function anim16:update(x, y, r, sx, sy)
+    -- Updating Image Arguments [ If any ] 
     self.x = x
     self.y = y
     self.r = r
@@ -47,6 +60,8 @@ function anim16:update(x, y, r, sx, sy)
     self.sy = sy
 end
 
+-- ==================================================================
+-- Drawing the Image
 function anim16:draw()
     lg.draw(self.c, self.x, self.y,self.r,self.sx,self.sy) -- Making animation visible
 end
